0f9285d10bf6d94a100a4589d8c16e029d556665
dekaef <mr.o.kamyshanov@gmail.com>
2024-12-22 20:48:34 +0300

    Лабораторная работа №4

---
+++ Лабораторная работа 4/task2_4.py
@@ -0,0 +1,25 @@
+# TODO импортировать необходимые молули
+
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+def task() -> None:
+    # Читаем содержимое csv файла
+    with open(INPUT_FILENAME, mode='r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = [row for row in reader]  # Преобразуем каждую строку в словарь
+
+    # Сериализуем данные в JSON файл с отступами равными 4
+    with open(OUTPUT_FILENAME, mode='w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4, ensure_ascii=False)
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
